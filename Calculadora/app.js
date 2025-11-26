d476903d195463066e6897d754439e902b33077f
Cesarmc-0 <montealegrecesar>
2025-11-26 16:39:30 -0500

    Estilos de Calculadora implementados

---
+++ Calculadora/app.js
@@ -0,0 +1,17 @@
+let resultado = document.getElementById('resultado');
+const operadores = document.getElementsByClassName('operador');
+const numeros = document.getElementsByClassName('numero');
+
+
+
+
+
+operadores.addEventListener('click', function () {
+
+})
+
+numeros.addEventListener('click', function () {
+
+
+})
+
